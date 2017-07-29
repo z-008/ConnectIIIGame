684728cd41ffc4fa5c3617624517b77dc5f5e75a
Sandeep Samant <sandeepsamant1702@gmail.com>
2017-07-29 15:26:46 +0530

    Added animations

--- app/src/main/java/com/example/apple/connectiiigame/MainActivity.java
+++ app/src/main/java/com/example/apple/connectiiigame/MainActivity.java
@@ -2,8 +2,25 @@ package com.example.apple.connectiiigame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view)
+    {
+      ImageView img = (ImageView) view;
+
+      
+        img.setTranslationY(-1000f);
+        img.animate().rotation(100f).translationYBy(1000f);
+
+        img.setImageResource(R.drawable.red);
+
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
